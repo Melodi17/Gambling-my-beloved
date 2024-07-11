fd2e4312803853ad8f13a8e2fc74b67724936d3a
Melodi <62381002+Melodi17@users.noreply.github.com>
2024-07-11 14:25:59 +1000

    Fixed total investment calculation

--- Models/StockOwnership.cs
+++ Models/StockOwnership.cs
@@ -31,10 +31,41 @@ public class StockOwnership
     {
         decimal totalInvestment = 0;
         
-        foreach (Transaction transaction in this.Transactions)
-            totalInvestment += transaction.Type == TransactionType.Buy 
-                ? transaction.Amount 
-                : -transaction.Amount;
+        // Store a list of stocks currently owned induvidually
+        List<(decimal cost, int amount)> ownedStocks = new();
+        
+        foreach (Transaction transaction in this.Transactions
+                     .OrderBy(transaction => transaction.Date))
+        {
+            if (transaction.Type == TransactionType.Buy)
+            {
+                totalInvestment += transaction.Amount;
+                decimal singleCost = transaction.Amount / transaction.Quantity;
+                ownedStocks.Add((singleCost, transaction.Quantity));
+            }
+            else
+            {
+                int quantity = transaction.Quantity;
+                
+                while (quantity > 0)
+                {
+                    (decimal cost, int amount) = ownedStocks[0];
+                    
+                    if (amount <= quantity)
+                    {
+                        totalInvestment -= cost * amount;
+                        quantity -= amount;
+                        ownedStocks.RemoveAt(0);
+                    }
+                    else
+                    {
+                        totalInvestment -= cost * quantity;
+                        ownedStocks[0] = (cost, amount - quantity);
+                        quantity = 0;
+                    }
+                }
+            }
+        }
         
         return totalInvestment;
     }

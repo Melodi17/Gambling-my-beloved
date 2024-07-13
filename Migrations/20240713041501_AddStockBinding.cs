db0aa5b8bf0ed78c0cf281f9e1d35963626fa0b5
Melodi <62381002+Melodi17@users.noreply.github.com>
2024-07-13 14:15:50 +1000

    Migrate stockbindings into table

---
+++ Migrations/20240713041501_AddStockBinding.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gambling_my_beloved.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStockBinding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StockBindings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StockId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false),
+                    BindTarget = table.Column<string>(type: "TEXT", nullable: true),
+                    Multiplier = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockBindings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StockBindings_Stocks_StockId",
+                        column: x => x.StockId,
+                        principalTable: "Stocks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StockBindings_StockId",
+                table: "StockBindings",
+                column: "StockId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StockBindings");
+        }
+    }
+}
